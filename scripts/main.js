31d60baca72634c14eaacc63ef2919fc0229b624
JHenderson219 <hendersonj219@gmail.com>
2017-04-10 15:43:13 -0700

    Complete ES6: Mutate an Array Declared with const

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,31 @@
-"use strict"
+"use strict";
+/*"use strict"
 const FAV_PET = "Cats";
 FAV_PET = "Dogs"; // returns error
+*/
+// ES6: Declare a Read-Only Variable with the const Keyword
+
+// change 'var' to 'let' or 'const'
+// rename constant variables
+const PI = 3.14;
+const RADIUS = 10;
+let calculateCircumference = function(r) {
+  let diameter = 2 * r;
+  let result = PI * diameter;
+  return result;
+};
+console.log(calculateCircumference(RADIUS));
+// Test your code
+
+//ES6: Mutate an Array Declared with const
+const s = [ 5, 7, 2 ];
+// change code below this line
+s[0] = 2;
+s[1] = 5;
+s[2] = 7;
+// change code above this line
+// Test your code
+console.log(s);
+
+
+document.getElementById("display").innerHTML = s;
