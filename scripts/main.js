b0302e8c3427ef41f83ebde49691aa08903434eb
JHenderson219 <hendersonj219@gmail.com>
2017-04-11 14:52:36 -0700

    Complete destructuring section

--- scripts/main.js
+++ scripts/main.js
@@ -26,7 +26,6 @@ s[2] = 7;
 // Test your code
 console.log(s);
 //ES6: Use Arrow Functions to Write Concise Anonymous Functions
-"use strict";
 // change code below this line
 const magic = () => {
   return new Date();
@@ -45,7 +44,73 @@ var squaredIntegers = realNumberArray.filter( e => e > 0).map(e => e*e);
 // change code above this line
 // test your code
 
+//ES6: Set Default Parameters for Your Functions
+function increment(number, value) {
+   return value ? number+value : number+1;
+}
 
 
+function sum(...arr) {
+    return arr.reduce((a, b) => a + b, 0);
+}
 
-document.getElementById("display").innerHTML = squaredIntegers;
+
+const arr1 = ['JAN', 'FEB', 'MAR', 'APR', 'MAY'];
+const arr2 = [];
+// change code below this line
+// change code above this line
+arr1.push('JUN');
+console.log(...arr1);
+
+//ES6: Use Destructuring Assignment to Assign Variables from Objects
+const greeting = 'itadakimasu';
+// change code below this line
+const { length } = greeting; // change this
+// change code above this line
+console.log(length); // should be using destructuring
+
+//ES6: Use Destructuring Assignment to Assign Variables from Nested Objects
+const forecast = {
+  today: { min: 72, max: 83 },
+  tomorrow: { min: 73.3, max: 84.6 }
+};
+// change code below this line
+const { tomorrow : {max: maxOfTomorrow} }  = forecast; // change this line
+// change code above this line
+console.log(maxOfTomorrow); // should be 84.6
+
+document.getElementById("display").innerHTML = length;
+
+//ES6: Use Destructuring Assignment to Assign Variables from Arrays
+let a = 8, b = 6;
+// change code below this line
+[a,b] = [6,8];
+// change code above this line
+console.log(a); // should be 6
+console.log(b); // should be 8
+
+//ES6: Use Destructuring Assignment with the Rest Operator to Reassign Array Elements
+const source = [1,2,3,4,5,6,7,8,9,10];
+// change code below this line
+const [,,...arr] = source; // change this
+// change code below this line
+console.log(arr); // should be [3,4,5,6,7,8,9,10]
+console.log(source); // should be [1,2,3,4,5,6,7,8,9,10];
+
+
+const stats = {
+  max: 56.78,
+  standard_deviation: 4.34,
+  median: 34.54,
+  mode: 23.87,
+  min: -0.75,
+  average: 35.85
+};
+// change code below this line
+const half = ({max,min}) => ((max + min) / 2.0); // use function argument destructurung
+// change code above this line
+console.log(stats); // should be object
+console.log(half(stats)); // should be 28.015
+
+
+document.getElementById("display").innerHTML = half(stats);
